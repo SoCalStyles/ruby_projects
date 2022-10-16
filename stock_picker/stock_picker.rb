792a0129ec3f4c67613e8d9b710e1d35198ef5c3
SoCalStyles <stylesaaronk@gmail.com>
2022-10-16 11:38:35 -0700

    remove first element if highest and last if lowest

---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,33 @@
+stock_prices = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(price_array)
+  (array.max == array[0]) ? array.shift : array
+  (array.min == array[-1]) ? array.pop : array
+
+
+
+# sort first from lowest to highest
+sorted_array = price_array.sort
+
+if price_array.index(sorted_array[0]) < price_array.index(sorted_array[-1])
+
+end
+
+price_array.reduce do |a, b|
+  a > b
+
+end
+
+# maybe loop until first occurence of first index being smaller than second
+
+
+# compare index of lowest and highest
+
+
+# compare first array element to make sure it's not the largest
+  array[0] 
+
+  # compare array indexes; ex: array.index(23) < array.index(3)
+
+end
+

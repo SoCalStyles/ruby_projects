25ed6118abab82ef79b120a03cc85b85a7cb671a
SoCalStyles <stylesaaronk@gmail.com>
2022-10-18 12:20:47 -0700

    pass thru array once; return largest at end

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,17 +1,12 @@
 def bubble_sort(array)
 
-  # take array and sort via bubble sort method
-
-  # compare each element to the one on the right
-  # loop n - 1 times
-  i = 0
-  while i < (array.length - 1)
-    array.each do |element|
-      if element > array.at(array.index(element) + 1)
-        # this is where the code goes to move element one spot to right
-      end
-    i += 1
+  # compare each element to one on the right
+  array.map do |element|
+    next if element == array[-1]
+    if element > array[array.index(element) + 1]
+      array.insert((array.index(element) + 1), array.delete(element))
     end
+    array
   end
   array
 end

aeda01544c9f7124dca87e3622c868ce03927e2b
SoCalStyles <stylesaaronk@gmail.com>
2022-10-19 16:26:05 -0700

    remove p;  add comments

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,13 +1,12 @@
 def bubble_sort(array)
   x = 0
   while x < (array.length - 1) do
-    array.each_with_index do |element, index|
+    array.each_with_index do |element, index| # compare each element to one on right
       break if index == (array.length - 1)
-      if element > array[index + 1]
+      if element > array[index + 1] # if larger than element on right, swap
         array.insert((index + 1), array.delete_at(index))
       end
     end
-  p array
   x += 1
   end
   array

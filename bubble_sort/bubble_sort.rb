15a5b5c959055b8a073775d6368c5fafa34b4de6
SoCalStyles <stylesaaronk@gmail.com>
2022-10-19 01:18:34 -0700

    implement loop for array - 1 times

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,12 +1,17 @@
 def bubble_sort(array)
-
+  x = 0
+  while x < (array.length - 1) do
   # compare each element to one on the right
   array.map do |element|
-    next if element == array[-1]
-    if element > array[array.index(element) + 1]
+    break if element == array[-1]
+    if element >= array[array.index(element) + 1]
+      # this below currently deletes duplicates
+      # is there another way we can tell computer to swap?
       array.insert((array.index(element) + 1), array.delete(element))
     end
-    array
+  end
+  p array
+  x += 1
   end
   array
 end

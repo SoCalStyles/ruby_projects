7b9ebabcae5b463c22ec4353ba3b063794389969
SoCalStyles <stylesaaronk@gmail.com>
2022-10-19 16:34:48 -0700

    add break condition if no swaps

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,13 +1,19 @@
 def bubble_sort(array)
   x = 0
   while x < (array.length - 1) do
+    swaps = 0
     array.each_with_index do |element, index| # compare each element to one on right
       break if index == (array.length - 1)
       if element > array[index + 1] # if larger than element on right, swap
         array.insert((index + 1), array.delete_at(index))
+        swaps += 1
       end
     end
+  p array
   x += 1
+  break if swaps == 0
   end
   array
 end
+
+# find out how to break if already sorted

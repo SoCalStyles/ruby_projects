21aa42d9d4ce12a9c94c324af0784052d10d2081
SoCalStyles <stylesaaronk@gmail.com>
2022-10-30 12:08:08 -0700

    determine winner; create new instance in main

--- tic_tac_toe/library.rb
+++ tic_tac_toe/library.rb
@@ -59,7 +59,60 @@ class Game
     determine_winner()
   end
 
-
-
+  def determine_winner()
+    if @board[0][0] == @board[0][1] && @board[0][1] == @board[0][2]
+      if @board[0][0] === 'X'
+        puts "#{@player1_name} has won the game!"
+      else
+        puts "#{@player2_name} wins the game!"
+      end
+    elsif @board[1][0] == @board[1][1] && @board[1][1] == @board[1][2]
+      if @board[1][0] === 'X'
+        puts "#{@player1_name} has won the game!"
+      else
+        puts "#{@player2_name} wins the game!"
+      end
+    elsif @board[2][0] == @board[2][1] && @board[2][1] == @board[2][2]
+      if @board[2][0] === 'X'
+        puts "#{@player1_name} has won the game!"
+      else
+        puts "#{@player2_name} wins the game!"
+      end
+    elsif @board[0][0] == @board[1][0] && @board[1][0] == @board[2][0]
+      if @board[0][0] === 'X'
+        puts "#{@player1_name} has won the game!"
+      else
+        puts "#{@player2_name} wins the game!"
+      end
+    elsif @board[0][1] == @board[1][1] && @board[1][1] == @board[2][1]
+      if @board[0][1] === 'X'
+        puts "#{@player1_name} has won the game!"
+      else
+        puts "#{@player2_name} wins the game!"
+      end
+    elsif @board[0][2] == @board[1][2] && @board[1][2] == @board[2][2]
+      if @board[0][2] === 'X'
+        puts "#{@player1_name} has won the game!"
+      else
+        puts "#{@player2_name} wins the game!"
+      end
+    elsif @board[0][0] == @board[1][1] && @board[1][1] == @board[2][2]
+      if @board[0][0] === 'X'
+        puts "#{@player1_name} has won the game!"
+      else
+        puts "#{@player2_name} wins the game!"
+      end
+    elsif @board[0][2] == @board[1][1] && @board[1][1] == @board[2][0]
+      if @board[0][2] === 'X'
+        puts "#{@player1_name} has won the game!"
+      else
+        puts "#{@player2_name} wins the game!"
+      end
+    elsif @board.flatten.count('X') == 5
+      puts "It's a tie game!"
+    else
+      player_choice()
+    end
+  end
 
 end

0531a942c2c6a3b7848f843aefdc733c6cafd6da
SoCalStyles <stylesaaronk@gmail.com>
2022-10-30 12:06:34 -0700

    add player choice method to Game class

--- tic_tac_toe/library.rb
+++ tic_tac_toe/library.rb
@@ -27,7 +27,37 @@ class Game
     player_choice()
   end
 
-
+  def player_choice()
+    if @board.flatten.count('X') == @board.flatten.count('O')
+      puts "#{@player1_name}'s turn, please choose a number to place an X"
+      @player1_choice = gets.chomp
+      @board = @board.map do |element|
+        element.map do |inner_element|
+          if inner_element === @player1_choice.to_s
+            inner_element = 'X'
+          else
+            inner_element = inner_element
+          end
+        end
+      end
+    else
+      puts "#{@player2_name}'s turn, please choose a number to place an O"
+      @player2_choice = gets.chomp
+      @board = @board.map do |element|
+        element.map do |inner_element|
+          if inner_element === @player2_choice.to_s
+            inner_element = 'O'
+          else
+            inner_element = inner_element
+          end
+        end
+      end
+    end
+    puts " #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]} "
+    puts " #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]} "
+    puts " #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]} "
+    determine_winner()
+  end
 
 
 

342bc20192942206ea361e31552fc81a90f73e71
SoCalStyles <stylesaaronk@gmail.com>
2022-10-30 12:05:44 -0700

    add create_board method to Game class

--- tic_tac_toe/library.rb
+++ tic_tac_toe/library.rb
@@ -9,6 +9,23 @@ class Game
     create_board()
   end
 
+  def create_board()
+    @board = Array.new(3) { Array.new(3, '-')}
+    @board[0][0] = '1'
+    @board[0][1] = '2'
+    @board[0][2] = '3'
+    @board[1][0] = '4'
+    @board[1][1] = '5'
+    @board[1][2] = '6'
+    @board[2][0] = '7'
+    @board[2][1] = '8'
+    @board[2][2] = '9'
+    puts "Here is your starting board:"
+    puts ' ' + @board[0][0] + ' | ' + @board[0][1] + ' | ' + @board[0][2]
+    puts ' ' + @board[1][0] + ' | ' + @board[1][1] + ' | ' + @board[1][2]
+    puts ' ' + @board[2][0] + ' | ' + @board[2][1] + ' | ' + @board[2][2]
+    player_choice()
+  end
 
 
 
